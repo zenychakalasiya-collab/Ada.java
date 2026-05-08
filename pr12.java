5aa162f3f24d12cd6c4a5e5148eb0216a0da7617
zenychakalasiya-collab <zenychakalasiya@gmail.com>
2026-05-08 14:01:46 +0530

    Create pr12.java

---
+++ pr12.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class LCSMatrix {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+       
+        System.out.print("Enter first string: ");
+        String s1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String s2 = sc.nextLine();
+
+        int m = s1.length();
+        int n = s2.length();
+
+        
+        int dp[][] = new int[m + 1][n + 1];
+
+        
+        for (int i = 1; i <= m; i++) {
+
+            for (int j = 1; j <= n; j++) {
+
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+
+                else {
+
+                    dp[i][j] = Math.max(dp[i - 1][j],
+                                        dp[i][j - 1]);
+                }
+            }
+        }
+
+       
+        System.out.println("\nLCS Matrix:");
+
+        for (int i = 0; i <= m; i++) {
+
+            for (int j = 0; j <= n; j++) {
+
+                System.out.print(dp[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+
+        
+        System.out.println("\nLength of LCS = " + dp[m][n]);
+
+        
+    }
+}
